a7abf0ab2e3b913d7fadb2f4fa4d4a611d445f6c
unknown <ayubfmohammed@gmail.com>
2024-09-11 18:06:37 -0600

    New changes

---
+++ sahaba/src/components/Donation.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Donation() {
+	  return (
+		      <div className="donation">
+		        <h1>Make a Donation</h1>
+		        <p>Your support helps us continue our important work.</p>
+		        <button>Donate Now</button>
+		      </div>
+		    );
+}
+
+export default Donation;
---
+++ sahaba/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+	  return (
+		      <div className="home">
+		        <h1>Welcome to Our Website</h1>
+		        <p>This is the home page of our amazing website.</p>
+		      </div>
+		    );
+}
+
+export default Home;
+
